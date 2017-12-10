45fcf6c9e217b17303630ddf8db6d81b65b8e074
Gaurav Dasgupta <dasguptag@HCL.COM>
2017-12-10 23:19:52 +0530

    Class Added For String Reversal Program

--- src/com/java/frequencycalculation/CharFrequencyCalculator.java
+++ src/com/java/frequencycalculation/CharFrequencyCalculator.java
@@ -1,3 +1,5 @@
+package com.java.frequencycalculation;
+
 import java.util.HashMap;
 import java.util.Map;
 
---
+++ src/com/java/reversestring/ReverseString.java
@@ -0,0 +1,21 @@
+package com.java.reversestring;
+
+/**
+ * Created by dasguptag on 10-12-2017.
+ */
+public class ReverseString {
+
+
+    public static void main(String args[]){
+        String input="TAVANTINPUT";
+        System.out.println("INPUT String-->"+ input);
+        byte[] inputBytes = input.getBytes();
+        byte[] outputBytes= new byte[input.length()];
+
+        for(int i=0; i<input.length();i++){
+            outputBytes[i]= inputBytes[input.length()-i-1];
+        }
+
+        System.out.println("Reversed String-->"+ new String(outputBytes));
+    }
+}
